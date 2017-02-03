2967125184cc58727a5ab58990ad784c455b35bf
amato19 <amato19@up.edu>
2017-02-02 20:24:55 -0800

    checkpoint2

--- app/src/main/java/com/example/amato19/carstereo/MainActivity.java
+++ app/src/main/java/com/example/amato19/carstereo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.amato19.carstereo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
@@ -11,13 +12,14 @@ import android.widget.ToggleButton;
 public class MainActivity extends AppCompatActivity {
 
     private ToggleButton power;
-    private Button am;
-    private Button fm;
+    private ToggleButton amfm;
+    //private Button fm;
     private Button volUp;
     private Button volDown;
     private TextView stationName;
     private SeekBar tuner;
-
+    private  double step;
+    private double min;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +27,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         power = (ToggleButton)findViewById(R.id.powerButton);
-        am = (Button)findViewById(R.id.amButton);
-        fm = (Button)findViewById(R.id.fmButton);
+        amfm = (ToggleButton)findViewById(R.id.amfmButton);
+        //fm = (Button)findViewById(R.id.fmButton);
         volUp = (Button)findViewById(R.id.upButton);
         volDown = (Button)findViewById(R.id.downButton);
         stationName = (TextView)findViewById(R.id.stationView);
@@ -37,16 +39,16 @@ public class MainActivity extends AppCompatActivity {
                 if (isChecked)
                 {
                     power.setTextColor(0xFFFFFFFF);
-                    am.setTextColor(0xFFFFFFFF);
-                    fm.setTextColor(0xFFFFFFFF);
+                    amfm.setTextColor(0xFFFFFFFF);
+                    //fm.setTextColor(0xFFFFFFFF);
                     volUp.setTextColor(0xFFFFFFFF);
                     volDown.setTextColor(0xFFFFFFFF);
                     stationName.setTextColor(0xFFFFFFFF);
                 } else
                 {
                     power.setTextColor(0x00000000);
-                    am.setTextColor(0x00000000);
-                    fm.setTextColor(0x00000000);
+                    amfm.setTextColor(0x00000000);
+                    //fm.setTextColor(0x00000000);
                     volUp.setTextColor(0x00000000);
                     volDown.setTextColor(0x00000000);
                     stationName.setTextColor(0x00000000);
@@ -54,8 +56,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-            final int step = 10;
-            final int min = 520;
+
+        amfm.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                {
+                    amfm.setText("AM");
+                    step = 11.7;
+                    min = 530;
+                } else
+                {
+                    amfm.setText("FM");
+                    step = 0.198;
+                    min = 88.1;
+
+                }
+            }
+        });
+
+
+
             tuner.setOnSeekBarChangeListener(
                 new SeekBar.OnSeekBarChangeListener()
                 {
@@ -69,8 +89,8 @@ public class MainActivity extends AppCompatActivity {
                     public void onProgressChanged(SeekBar seekBar, int progress,
                                                   boolean fromUser)
                     {
-                        double value = min + (progress + step);
-                        stationName.setText("" + (int)value);
+                        double value = min + (progress * step);
+                        stationName.setText("" + value);
                     }
                 }
         );
